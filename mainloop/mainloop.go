9035f2f3b94cca0159346f3ef5c73ee76e5f6ab1
Emad Heravi <eheravi@in2tions.com>
2025-11-27 14:44:59 +0100

    first test

--- mainloop/mainloop.go
+++ mainloop/mainloop.go
@@ -238,8 +238,7 @@ main:
 			m.statusLock.Unlock()
 
 			// Wrap raw UDP packet into RistDataBlock for outputs
-			rb := &libristwrapper.RistDataBlock{}
-			rb.Data = append([]byte(nil), pkt.Data...)
+			rb := libristwrapper.NewRistDataBlock(pkt.Data)
 
 			m.writeOutputs(rb)
 
