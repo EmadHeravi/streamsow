af57f762ab4b780124601a2516df372031a48e5c
Emad Heravi <eheravi@in2tions.com>
2025-11-28 18:26:32 +0100

    first test

--- include_srt/libristwrapper/utils.go
+++ include_srt/libristwrapper/utils.go
@@ -6,6 +6,12 @@ package libristwrapper
 */
 import "C"
 
+// LibraryVersion returns the linked librist library version string.
 func LibraryVersion() string {
-	return C.GoString(C.rist_version())
+	return C.GoString(C.librist_version())
+}
+
+// LibraryAPIVersion returns the librist API version string.
+func LibraryAPIVersion() string {
+	return C.GoString(C.librist_api_version())
 }
